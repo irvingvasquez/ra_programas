9685cd0924469a521d020f4af7710ef145056be6
JI Vasquez <jirvingvg@gmail.com>
2018-03-01 11:35:39 -0600

    updated exercise

--- ra_03_trans/euler_rot_ejercicio.py
+++ ra_03_trans/euler_rot_ejercicio.py
@@ -1,4 +1,4 @@
-Rotaciones de Euler.
+#Rotaciones de Euler.
 
 # In this notebook you'll explore Euler rotations and get a feel for why the order of rotations matters.
 # Euler rotations as we define them in this program are counterclockwise about the axes of the vehicle body frame, where:
@@ -9,50 +9,57 @@ Rotaciones de Euler.
 
 #As you'll see the same set of rotation transformations, applied in a different order can produce a very different final result!
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-​
 import numpy as np
 from enum import Enum
-​
-%matplotlib inline
+
+#%matplotlib inline
 plt.rcParams["figure.figsize"] = [12, 12]
 np.set_printoptions(precision=3, suppress=True)
 
 class Rotation(Enum):
    ROLL = 0
    PITCH = 1
-   YAW = 2  
-​​
+   YAW = 2
+
 class EulerRotation:
    def __init__(self, rotations):
-   """
-   `rotations` is a list of 2-element tuples where the
-   first element is the rotation kind and the second element
-   is angle in degrees.
+      '''
+      rotations` is a list of 2-element tuples where the
+      first element is the rotation kind and the second element
+      is angle in degrees.
         
-   Ex:
-     [(Rotation.ROLL, 45), (Rotation.YAW, 32), (Rotation.PITCH, 55)]
-   """
-   self._rotations = rotations
-   self._rotation_map = {Rotation.ROLL : self.roll, Rotation.PITCH : self.pitch, Rotation.YAW : self.yaw}
-​
+      Ex:
+        [(Rotation.ROLL, 45), (Rotation.YAW, 32), (Rotation.PITCH, 55)]
+      '''
+
+      self._rotations = rotations
+      self._rotation_map = {Rotation.ROLL : self.roll, Rotation.PITCH : self.pitch, Rotation.YAW : self.yaw}
+
    def roll(self, phi):
-      """Returns a rotation matrix along the roll axis"""
-      return 
+      '''Returns a rotation matrix along the roll axis'''
+      # TODO:
+      return R 
     
    def pitch(self, theta):
       """Returns the rotation matrix along the pitch axis"""
-      return 
-​
+      # TODO:
+      return R
+
    def yaw(self, psi):
       """Returns the rotation matrix along the yaw axis"""
-      return 
-​
+      # TODO:
+      return R
+
    def rotate(self):
-      """Applies the rotations in sequential order"""
-      t = np.eye(3)
-      return t
+      """Applies the rotations in sequential order
+         Using first the rot over z, then y, then x
+         remember that you are rotating an object
+      """
+      R = np.eye(3)
+      # TODO:
+      return R
 
 # Below you'll form a sequence of rotations. The rotation matrix R is the mapping of performing the rotations in sequential order. Multiplying a vector by R will perform the rotations on that vector.
 
@@ -62,7 +69,13 @@ rotations = [
     (Rotation.PITCH, 75),
     (Rotation.YAW, 90),
 ]
-​
+
+#for rot, angle in rotations:
+#   print(rot)
+#   print(angle)
+
+#print(rotations)
+
 R = EulerRotation(rotations).rotate()
 print('Rotation matrix ...')
 print(R)
@@ -82,45 +95,53 @@ print(R)
 R1 = None
 R2 = None
 R3 = None
+
 # Seeing is Believing
 # It's clear the three rotation matrices are different, but, in order to get an deeper understanding let's plot the impact of the rotations.
 
 # unit vector along x-axis
 v = np.array([1, 0, 0])
-Apply the rotations to v. Feel free to change the values of this input vector.
-In [ ]:
+
+#Apply the rotations to v. Feel free to change the values of this input vector.
 
 # TODO: calculate the new rotated versions of `v`.
+# rv = np.dot(R, v)
 rv1 = None
 rv2 = None
-rv3 = None
-# rv = np.dot(R, v)
-Plotting time ...
+rv3 = None 
+#Plotting time ...
+print('Rotated vectors ...')
+print(rv1)
+print(rv2)
+print(rv3)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-​
+
 # axes (shown in black)
 ax.quiver(0, 0, 0, 1.5, 0, 0, color='black', arrow_length_ratio=0.15)
 ax.quiver(0, 0, 0, 0, 1.5, 0, color='black', arrow_length_ratio=0.15)
 ax.quiver(0, 0, 0, 0, 0, 1.5, color='black', arrow_length_ratio=0.15)
-​​
+
 # Original Vector (shown in blue)
 ax.quiver(0, 0, 0, v[0], v[1], v[2], color='blue', arrow_length_ratio=0.15)
-​
+
 # Rotated Vectors (shown in red)
-ax.quiver(0, 0, 0, rv1[0], rv1[1], rv1[2], color='red', arrow_length_ratio=0.15)
-ax.quiver(0, 0, 0, rv2[0], rv2[1], rv2[2], color='purple', arrow_length_ratio=0.15)
-ax.quiver(0, 0, 0, rv3[0], rv3[1], rv3[2], color='green', arrow_length_ratio=0.15)
-​
+ax.quiver(0, 0, 0, rv1.item((0, 0)), rv1.item((0, 1)), rv1.item((0, 2)), color='red', arrow_length_ratio=0.15)
+ax.quiver(0, 0, 0, rv2.item((0, 0)), rv2.item((0, 1)), rv2.item((0, 2)), color='purple', arrow_length_ratio=0.15)
+ax.quiver(0, 0, 0, rv3.item((0, 0)), rv3.item((0, 1)), rv3.item((0, 2)), color='green', arrow_length_ratio=0.15)
+print(rv1)
+print(rv2)
+print(rv3)
+
 ax.set_xlim3d(-1, 1)
 ax.set_ylim3d(1, -1)
 ax.set_zlim3d(1, -1)
-​
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-​
+
 plt.show()
 
 #Gimbal Lock
